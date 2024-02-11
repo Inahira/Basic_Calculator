a3979eb1c85a41a54d6123279e0b8bb5104242dd
Tahira inam <sololearner120@gmai.com>
2024-02-11 07:43:24 -0800

    C++ Calculator using if-else statemnts

---
+++ c++ calculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b, c;
+	char op;
+	
+	cout<<"Enter 1st number:\n";
+	cin>>a;
+	cout<<"Enter 2nd number:\n";
+	cin>>b;
+	
+	cout<<"Enter Operator\n";
+	cin>>op;
+	
+	if (op == '+')
+	{
+		c = a + b;
+		cout << a <<" + "<< b <<" = "<<c<<endl;
+		
+	}
+	else if (op == '-')
+	{              
+		c = a - b;
+		cout << a <<" - "<< b <<" = "<<c<<endl;
+	}
+	else if (op == '*')
+	{              
+		c = a * b;
+		cout << a <<" * "<< b <<" = "<<c<<endl;
+	}
+	else if (op == '/')
+	{               
+		c = a / b;
+		cout << a <<" / "<< b <<" = "<<c<<endl;
+	}
+	else
+	{
+		cout<<"This C++ calculator can only perform \'+\', \'-\', \'*\', \'/\' operations\n";
+	}
+	return 0;
+}
